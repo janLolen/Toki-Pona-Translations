5b63723b8d5c2ac0e3568fef76239536fed20a9e
Eic17H <139933730+Eic17H@users.noreply.github.com>
2024-07-19 12:50:54 +0200

    Minecraft: nimi sin li kama

--- Minecraft/JSON_updater.py
+++ Minecraft/JSON_updater.py
@@ -1,9 +1,9 @@
 import json
 
-with open('en_us.json', encoding='UTF-8') as old:
-    with open('tok.json', encoding='UTF-8') as new:
-        oata = json.load(old)
-        nata = json.load(new)
+with open('tok_Lasina.json', encoding='UTF-8') as old:
+    with open('tok_pona.json', encoding='UTF-8') as new:
+        oata = json.load(old) # data that will be used only if not in the other one
+        nata = json.load(new) # all data here will be kept, this file is supposed to be missing entries
         for i in nata:
             oata[i] = nata[i]
 
