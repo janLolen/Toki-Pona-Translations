1af8d8d122ea3971929ddf4c13a6383880f2484b
jan Lolen <163869597+janLolen@users.noreply.github.com>
2025-01-19 20:14:33 +0100

    nimi jan li kama ante

--- Minecraft/resource pack/clone.bat
+++ Minecraft/resource pack/clone.bat
@@ -1,4 +1,4 @@
-robocopy .\assets "C:\Users\eic17\AppData\Roaming\.minecraft\resourcepacks\poki pi sitelen pona\assets" /E
-robocopy .\assets "C:\Users\eic17\AppData\Roaming\.minecraft\resourcepacks\poki pi sitelen telo\assets" /E
-del "C:\Users\eic17\AppData\Roaming\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\textures\font\sitelen_pona.png"
-rename "C:\Users\eic17\AppData\Roaming\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\textures\font\sitelen_telo.png" "sitelen_pona.png"
+robocopy .\assets "C:\Users\UTENTE\AppData\Roaming\.minecraft\resourcepacks\poki pi sitelen pona\assets" /E
+robocopy .\assets "C:\Users\UTENTE\AppData\Roaming\.minecraft\resourcepacks\poki pi sitelen telo\assets" /E
+del "C:\Users\UTENTE\AppData\Roaming\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\textures\font\sitelen_pona.png"
+rename "C:\Users\UTENTE\AppData\Roaming\.minecraft\resourcepacks\poki pi sitelen telo\assets\minecraft\textures\font\sitelen_telo.png" "sitelen_pona.png"
